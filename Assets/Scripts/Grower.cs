ae8e76dcd202075ef63574bf3ccf50339ece985a
Sara18Santos <sataide.santos@gmail.com>
2025-05-23 19:15:10 +0100

    some things for l-systems

---
+++ Assets/Scripts/Grower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Generated(int i )
+    {
+        this.transform.position += Vector3.up;
+    }
+    // {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+}
---
+++ Assets/Scripts/RecursiveInstaciator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class RecursiveInstaciator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int recurse = 5;
+    public int splitNumber = 2;
+    public Vector3 pivotPosition;
+    void Start()
+    {
+        recurse -=1;
+        for(int i = 0; i < splitNumber; i++)
+        {
+            if (recurse > 0)
+            {
+                var copy = Instantiate(gameObject);
+                var recursive = copy.GetComponent<RecursiveInstaciator>();
+                recursive.SendMessage("Generated", i);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
