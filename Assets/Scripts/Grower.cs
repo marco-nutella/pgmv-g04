7db9d13c75fbc1f870ff3033debfa78f4bb77a3e
Sara18Santos <sataide.santos@gmail.com>
2025-05-23 16:40:54 +0100

    update

---
+++ Assets/Scripts/Grower.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position += Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RecursiveInstaciator.cs
+++ Assets/Scripts/RecursiveInstaciator.cs
@@ -1,14 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class RecursiveInstaciator : MonoBehaviour
 {
     // Start is called before the first frame update
     public int recursive = 5;
+    public int splitNumber = 2;
+    public Vector3 pivotPosition;
     void Start()
     {
-        
+        recursive -=1;
+        for(int i = 0; i < splitNumber; i++)
+        {
+            if (recursive > 0)
+            {
+                var copy = Instantiate(gameObject);
+                var recursive = copy.GetComponent<RecursiveInstaciator>();
+                recursive.SendMessage("Generatee");
+            }
+        }
     }
 
     // Update is called once per frame
