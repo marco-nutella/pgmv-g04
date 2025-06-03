c8cb1585b32239f585d396a27df7e9d08fc56b96
tiagof2001 <159270739+tiagof2001@users.noreply.github.com>
2025-06-03 12:35:37 +0100

    update
    atualização do ficheiro para interface da planta

--- Assets/Scripts/PlantInterface.cs
+++ Assets/Scripts/PlantInterface.cs
@@ -5,21 +5,22 @@ using UnityEngine.UI;
 
 public class PlantInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
     public InputField axiomaInput;
     public InputField iterationsInput;
     public InputField lengthInput;
     public InputField minAngleInput;
     public InputField maxAngleInput;
-    public InputField velocityWind;
+    public InputField speedWindInput;
+
+    //adicionar velocidade do vento
+
 
     public Button playButton;
     public Button pauseButton;
     public Button restartButton;
     public Toggle windToggle;
 
-    private PlantGenerator selectedPlant;
-    private GeradorPlantas selectedPlant;
+    //private GeradorPlantas selectedPlant;
 
     void Start()
     {
@@ -27,25 +28,42 @@ public class PlantInterface : MonoBehaviour
         playButton.onClick.AddListener(OnPlay);
         pauseButton.onClick.AddListener(OnPause);
         restartButton.onClick.AddListener(OnRestart);
-        windToggle.onValueChanged.AddListener(OnToggleWind);
         windToggle.onValueChanged.AddListener(OnToggleWind);*/
     }
-
-    public void SetSelectedPlant(PlantGenerator plant)
+    /**
     public void SetSelectedPlant(GeradorPlantas plant)
     {
+        
         selectedPlant = plant;
 
+        // Preencher os campos com os valores atuais da planta
+        axiomaInput.text = plant.axiom;
+        iterationsInput.text = plant.iterations.ToString();
+        lengthInput.text = plant.length.ToString();
+        minAngleInput.text = plant.minAngle.ToString();
+        maxAngleInput.text = plant.maxAngle.ToString();
+        //adicionar velocidade do vento
+        
+    }*/
 
-
-@@ -58,8 +59,9 @@ public class PlantInterface : MonoBehaviour
+    void Update()
+    {
+        /**
+        if (selectedPlant != null)
+        {
+            // Atualizar parâmetros em tempo real
+            selectedPlant.axiom = axiomaInput.text;
+            int.TryParse(iterationsInput.text, out selectedPlant.iterations);
+            float.TryParse(lengthInput.text, out selectedPlant.length);
+            float.TryParse(minAngleInput.text, out selectedPlant.minAngle);
+            float.TryParse(maxAngleInput.text, out selectedPlant.maxAngle);
+        }*/
     }
 
-    //Necessario criar funções para play, pause, restart
     /**
     void OnPlay() => selectedPlant?.PlayGrowth();
     void OnPause() => selectedPlant?.PauseGrowth();
     void OnRestart() => selectedPlant?.RestartGrowth();
     void OnToggleWind(bool isOn) => selectedPlant?.SetWind(isOn);
-    void OnToggleWind(bool isOn) => selectedPlant?.SetWind(isOn);*/
+    */
 }
