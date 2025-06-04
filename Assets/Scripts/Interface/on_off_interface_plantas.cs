006e515b3044b23753e14c20cfb1232a827f5f20
tiagof2001 <159270739+tiagof2001@users.noreply.github.com>
2025-06-04 16:18:49 +0100

    update
    atualização da interface

--- Assets/Scripts/Interface/on_off_interface_plantas.cs
+++ Assets/Scripts/Interface/on_off_interface_plantas.cs
@@ -12,11 +12,19 @@ public class openCloseInterface : MonoBehaviour
     public GameObject interfaceHelp;
 
     // Gameobject do robô
-    [SerializeField] public GameObject robot;
+    [SerializeField] GameObject robot;
+    [SerializeField] Camera targetCamera;
+    public LayerMask[] cullingOptions;
+    private int currentIndex = 0;
 
     private PlayerController playerController;
     private GenerateArm roboInteration;
+    private RoboOscilação roboOscilacao;
+
     private GameObject holdPoint;
+
+    private Vector3 holdPointOriginalLocalPos;
+    private float deslocamentoLateral = 2.5f;
 
     void Start()
     {
@@ -26,19 +34,46 @@ public class openCloseInterface : MonoBehaviour
         GameObject mainObject = robot; // O objeto principal
         playerController = mainObject.GetComponent<PlayerController>();
         roboInteration = mainObject.GetComponent<GenerateArm>();
+        roboOscilacao = mainObject.GetComponentInChildren<RoboOscilação>();
         holdPoint = GameObject.Find("Ponto_Agarrar");
 
         if (playerController == null) Debug.LogError("PlayerController não encontrado!");
         if (roboInteration == null) Debug.LogError("GenerateArm não encontrado!");
+        if (roboOscilacao == null) Debug.LogError("RoboOscilação não encontrado!");
         if (holdPoint == null) Debug.LogError("HoldPoint não encontrado!");
+        else holdPointOriginalLocalPos = holdPoint.transform.localPosition;
     }
     
+    public void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) // Exemplo: Abrir/fechar interface com ESC
         {
             if (holdPoint.transform.childCount > 0)
             {
+                currentIndex = (currentIndex + 1) % cullingOptions.Length;
+                targetCamera.cullingMask = cullingOptions[currentIndex];
+                Debug.Log("Culling Mask changed to option: " + currentIndex);    
+
+                if(!interfacePanel.activeSelf){
+                    //holdPoint.layer = LayerMask.NameToLayer("ManipulacaoPlanta");
+                    SetLayerRecursively(holdPoint,LayerMask.NameToLayer("ManipulacaoPlanta"));
+                    holdPoint.transform.localPosition = holdPointOriginalLocalPos + new Vector3(deslocamentoLateral, 0f, 0f);
+
+                } else {
+                    //holdPoint.layer = LayerMask.NameToLayer("Default");
+                    SetLayerRecursively(holdPoint,LayerMask.NameToLayer("Default"));
+                    holdPoint.transform.localPosition = holdPointOriginalLocalPos;
+                }
+
                 PlantInterface plantInterface = interfacePanel.GetComponent<PlantInterface>();
                 plantInterface.getDadosPlanta(holdPoint);
 
@@ -48,6 +83,7 @@ public class openCloseInterface : MonoBehaviour
                 // Bloqueia ou desbloqueia o movimento do player
                 playerController.blockMoveInteraction(!interfacePanel.activeSelf);
                 roboInteration.blockMoveInteraction(!interfacePanel.activeSelf);
+                roboOscilacao.blockMoveInteraction(!interfacePanel.activeSelf);
 
                 //Desativa a interface de ajuda
                 interfaceHelp.SetActive(false);
@@ -55,7 +91,6 @@ public class openCloseInterface : MonoBehaviour
             } else {
                 Debug.LogError("Robô não está a segurar numa planta");
             }
-
         }
 
         if (Input.GetKeyDown(KeyCode.H)) // Exemplo: Abrir/fechar interface com ESC
