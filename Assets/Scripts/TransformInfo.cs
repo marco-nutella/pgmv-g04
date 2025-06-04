cc3a7ec885a14a20dae3abe27dad130c5457bc7f
Marco Pinho <marcoantonio.mp.camargo@gmail.com>
2025-06-04 19:40:35 +0100

    FIXED THE GODDAMN DOORS AGAIN

---
+++ Assets/Scripts/TransformInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformInfo : MonoBehaviour
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
